954843f3125b3af3c6d901cb99bb7f44c52b8848
Steve Mostovoy <steve.mostovoy@coinbase.com>
2019-10-23 13:24:32 -0700

    Updates

---
+++ precache-manifest.f808addb8a311a96b0c97c4e23c34e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4070b4548856644bb1dc36f66cb3f7e",
+    "url": "/react-presentation/static/media/calculator-no-style.f4070b45.png"
+  },
+  {
+    "revision": "f5c0c7248f8b1c0bcff8",
+    "url": "/react-presentation/static/js/runtime~main.f5c0c724.js"
+  },
+  {
+    "revision": "b87d475e81dea8d71773",
+    "url": "/react-presentation/static/js/main.b87d475e.chunk.js"
+  },
+  {
+    "revision": "e9f5e0714426859d8f6f",
+    "url": "/react-presentation/static/js/1.e9f5e071.chunk.js"
+  },
+  {
+    "revision": "e9f5e0714426859d8f6f",
+    "url": "/react-presentation/static/css/1.ac3da55e.chunk.css"
+  },
+  {
+    "revision": "4697e8738d08f6e42020a858fb2ca41e",
+    "url": "/react-presentation/index.html"
+  }
+];
